0ad154a97d32bac06071f63cb716b2c014f643f3
Akshay Narayan <akshayn@mit.edu>
2018-01-21 19:12:27 -0500

    faster algorithm which disregards curve end

--- frechet.go
+++ frechet.go
@@ -26,40 +26,27 @@ func min(x float64, y float64, z float64) float64 {
 func Frechet(c1 []Point, c2 []Point) float64 {
 	I := len(c1)
 	J := len(c2)
-	res := make([][]float64, I)
-	for i := range res {
-		res[i] = make([]float64, J)
-		for j := range res[i] {
-			res[i][j] = -1.0
-		}
-	}
-
+	runningMaxI := 0.0
 	for i := 0; i < I; i++ {
+		currentMin := 1e+09
 		for j := 0; j < J; j++ {
-			if res[i][j] > -1 {
-				return res[i][j]
-			}
-
-			if i == 0 && j == 0 {
-				res[i][j] = euclideanDistance(c1[0], c2[0])
-			} else if i > 0 && j == 0 {
-				res[i][j] = math.Max(res[i-1][0], euclideanDistance(c1[i], c2[0]))
-			} else if i == 0 && j > 0 {
-				res[i][j] = math.Max(res[0][j-1], euclideanDistance(c1[0], c2[j]))
-			} else if i > 0 && j > 0 {
-				res[i][j] = math.Max(
-					min(
-						res[i-1][j-1],
-						res[i][j-1],
-						res[i-1][j],
-					),
-					euclideanDistance(c1[i], c2[j]),
-				)
-			} else {
-				res[i][j] = math.Inf(1)
-			}
+			currDist := euclideanDistance(c1[i], c2[j])
+			currentMin = math.Min(currentMin, currDist)
 		}
+
+		runningMaxI = math.Max(runningMaxI, currentMin)
 	}
 
-	return res[I-1][J-1]
+	runningMaxJ := 0.0
+	for j := 0; j < J; j++ {
+		currentMin := 1e+09
+		for i := 0; i < I; i++ {
+			currDist := euclideanDistance(c1[i], c2[j])
+			currentMin = math.Min(currentMin, currDist)
+		}
+
+		runningMaxJ = math.Max(runningMaxJ, currentMin)
+	}
+
+	return math.Max(runningMaxI, runningMaxJ)
 }
